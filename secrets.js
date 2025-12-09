be350d6c2c87314fe9bd1b8aba90249ee5fc2f2e
pakkemaen <125302635+pakkemaen@users.noreply.github.com>
2025-12-09 15:01:11 +0100

    Add files via upload

---
+++ secrets.js
@@ -0,0 +1,20 @@
+// secrets.js
+// DIT BESTAND BEVAT GEVOELIGE SLEUTELS. DEEL DIT NIET ONLINE.
+
+const CONFIG = {
+    // Jouw Google AI Key (voor recepten)
+    apiKey: "AIzaSyDsyQ3PblMaB4qyHuwpTNg4E45yKdlkI4Y", 
+
+    // Jouw Firebase Config
+    firebase: {
+        apiKey: "AIzaSyDsyQ3PblMaB4qyHuwpTNg4E45yKdlkI4Y", 
+        authDomain: "meandery-aa05e.firebaseapp.com",
+        projectId: "meandery-aa05e",
+        storageBucket: "meandery-aa05e.appspot.com",
+        messagingSenderId: "388311971225",
+        appId: "1:388311971225:web:e5b0e81ce18d96b4a88f08",
+        measurementId: "G-S5CPLP80XT"
+    }
+};
+
+export default CONFIG;
